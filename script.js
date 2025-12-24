36a899affd5ce023f5cea1bb2937ad09ad49232c
Damir-Fullstack <ortukovdamir773@gmail.com>
2025-12-24 10:01:13 +0100

    Add random background color on button toggle
    Change background color randomly on button click.

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const output =
 document.getElementById("output");
 btn.addEventListener("click", () =>{
   state = !state;
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 if (state) {
   output.innerText = "Hello World!";
+  document.body.style.backgroundColor = randomColor;
 } else {
   output.innerText = "";
+  document.body.style.backgroundColor = randomColor;
 }
 })
